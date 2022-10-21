28ea581a051e0ae0c56ba59d1a1402a0041f5f9b
Юлия Пшеничная <yulenykaa@gmail.com>
2022-10-21 10:25:09 +0300

    add hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const videoEl = document.querySelector('iframe');
+const player = new Player(videoEl);
+
+player.on(
+    'timeupdate',
+    throttle(event => {
+        localStorage.setItem('videoplayer-current-time', event.seconds);
+    }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(function (error) {
+    console.error(error);
+  });
